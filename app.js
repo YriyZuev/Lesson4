6e748479cd5a176e0495d1d756f7a230ba9588f4
Yriy <90506298+YriyZuev@users.noreply.github.com>
2021-09-12 19:32:25 +0300

    Lesson4

---
+++ app.js
@@ -0,0 +1,28 @@
+const board = document.querySelector('#board')
+const colors = ['#cc232ee5', '#cc2394e5', '#761485e5', '#3c18c9e5', '#14bde3e5', '#1fdbc5e5', '#21d933e5', '#cce815e5', '#d9870de5', '#c43616e5']
+const SQUARES_NUMBER = 500
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
